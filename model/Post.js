7c3b70122cd898544967d95e092674630da46b83
gusta <ovatsugavlis15@gmail.com>
2022-10-01 21:38:12 -0300

    correção de bugs

--- model/Post.js
+++ model/Post.js
@@ -1,9 +1,9 @@
-/* const knex = require('../database/knex') */
+const knex = require('../database/knex')
 class post {
     async PostVideo(data){
         let {name, url, key} = data
         try{
-           /*  let video = await knex.insert({nome:name, url, key}).into('post')*/
+            let video = await knex.insert({nome:name, url, key}).into('post')
             return video
         }catch(err){
             throw err
--- model/arquivo.js
+++ model/arquivo.js
@@ -12,29 +12,8 @@ async function arquivo (){
     }
 }
 async function Pastas() {
-    /*try{*/
-        /* let pastas = await fs.readdir(path.join('/', 'DW-Temps', 'doctor.who.S01')) */
-        /*let pastas =  await fs.readdir('C:/Users/paulo/OneDrive/Documents/gusta/')*/
-        /* pastas.forEach(async arquivo => {
-            let caminho = 'C:/Users/paulo/OneDrive/Documents/gusta/'
-            let ep = fs2.createReadStream(`${caminho}${arquivo}`) 
-            let dadosEstaticos = fs2.statSync(`${caminho}/${arquivo}`)
-            let obj = {
-                ep: ep,
-                dadosEstaticos: dadosEstaticos
-            }
-            console.log('O OBJETO', obj)
-            return new Promise((resolve, reject) => {
-                resolve(obj)
-                
-            })
-        })
-    }catch(err){
-        console.log('deu erro aq boy', err)
-        throw err
-    } */
     try {
-        let caminho = 'C:/Users/paulo/OneDrive/Documents/gusta/'
+        let caminho = 'D:/DW-Temps'
         let arquivo = await fs.readdir(caminho)
         let ep = fs2.createReadStream(`${caminho}${arquivo}`)
         let dadosEstaticos = fs2.statSync(`${caminho}${arquivo}`)
@@ -47,14 +26,8 @@ async function Pastas() {
     catch(err) {
         throw err
     }
-    
-
 }
 async function expressStart(ep){
-    return new Promise((resolve, reject)=>{
-        axios.post('http://localhost:3300/video', {file: ep}, {headers:{
-            "Content-Type": "multipart/form-data"
-        }}).then(params =>{
     return new Promise((resolve, reject)=>{
         axios({ method: 'post', 
         url: 'http://localhost:3300/video', 
@@ -82,23 +55,3 @@ async function StartUploads() {
     }
 }
 StartUploads()
-
-/*  
-    fieldname: 'file',
-    originalname: 'Gravar_2022_08_28_22_10_32_39.mp4',
-    encoding: '7bit',
-    mimetype: 'video/mp4',
-    size: 2147231,
-    bucket: 'amigitos-espanol-doctor-who',
-    key: 'Gravar_2022_08_28_22_10_32_39-a68415fe3026784ea11c.mp4',
-    acl: 'public-read',
-    contentType: 'video/mp4',
-    contentDisposition: null,
-    contentEncoding: null,
-    storageClass: 'STANDARD',
-    serverSideEncryption: null,
-    metadata: undefined,
-    location: 'https://amigitos-espanol-doctor-who.s3.sa-east-1.amazonaws.com/Gravar_2022_08_28_22_10_32_39-a68415fe3026784ea11c.mp4',
-    etag: '"a126cc466ae5eac0fc6c54bbad530cba"',
-    versionId: undefined 
-*/
